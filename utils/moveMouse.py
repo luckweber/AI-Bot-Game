b7ae17d15e587d727edb8f65b9f7e605a56a4b3d
Thiago Pereira <thiago.pereira@agencian1.com.br>
2020-11-22 00:04:19 -0300

    Init Commit

---
+++ utils/moveMouse.py
@@ -0,0 +1,23 @@
+# x, y = pyautogui.locateCenterOnScreen('calc7key.png')
+# print(y)
+
+# Get  Screen
+# print(pyautogui.size())
+
+# Get Mouse
+# print(pyautogui.position())
+
+# click the mouse
+# pyautogui.click()  
+# pyautogui.moveTo(100, 200)
+# pyautogui.click(x=2112, y=870,button='right')
+
+# print('Press Ctrl-C to quit.')
+# try:
+#     while True:
+#         x, y = pyautogui.position()
+#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+#         print(positionStr, end='')
+#         print('\b' * len(positionStr), end='', flush=True)
+# except KeyboardInterrupt:
+#     print('\n')
